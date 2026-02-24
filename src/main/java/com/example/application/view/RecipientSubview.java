e45ba0b7ef68dcea83b4e98cc187e4531ce70860
Petter Holmstrom <petter@vaadin.com>
2026-02-24 09:16:04 +0200

    Styling improvements

--- src/main/java/com/example/application/view/RecipientSubview.java
+++ src/main/java/com/example/application/view/RecipientSubview.java
@@ -6,9 +6,13 @@ import com.example.application.model.RecipientModel;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.signals.Signal;
 
 class RecipientSubview extends VerticalLayout {
@@ -19,34 +23,54 @@ class RecipientSubview extends VerticalLayout {
         this.letterModel = letterModel;
         var addButton = new Button("Add Recipient", e -> letterModel.addRecipient());
 
-        // TODO Fix scrolling, margins, etc.
         var recipients = new VerticalLayout();
         recipients.bindChildren(letterModel.recipients(), this::createRecipientCard);
+        recipients.setPadding(false);
+        recipients.setSpacing(true);
+        recipients.setDefaultHorizontalComponentAlignment(Alignment.STRETCH);
+        var scroller = new Scroller(recipients);
+        scroller.setSizeFull();
 
-        add(addButton, recipients);
+        add(addButton, scroller);
+        setSizeFull();
+        setPadding(false);
     }
 
     private Component createRecipientCard(Signal<Recipient> recipient) {
         var recipientModel = new RecipientModel(letterModel, recipient, letterModel::updateRecipient);
-        var card = new VerticalLayout();
+        var card = new Div();
+        card.addClassNames("recipient-card");
 
         var name = new TextField();
+        name.setPlaceholder("Name");
+        name.setValueChangeMode(ValueChangeMode.LAZY);
         name.bindValue(recipientModel.name(), recipientModel::setName);
         name.bindReadOnly(recipientModel.readOnly());
+        name.setWidthFull();
 
         var email = new EmailField();
+        email.setPlaceholder("Email");
+        email.setValueChangeMode(ValueChangeMode.LAZY);
         email.bindValue(recipientModel.emailAddress(), recipientModel::setEmailAddress);
         email.bindReadOnly(recipientModel.readOnly());
+        email.setWidthFull();
 
-        var requireResponse = new Checkbox();
+        var requireResponse = new Checkbox("Require Response");
         requireResponse.bindValue(recipientModel.requireResponse(), recipientModel::setRequireResponse);
         requireResponse.bindReadOnly(recipientModel.readOnly());
 
-        var remove = new Button("Remove", e -> recipientModel.remove());
+        var remove = new Button(VaadinIcon.CLOSE.create(),e -> recipientModel.remove());
         remove.bindVisible(Signal.not(recipientModel.readOnly()));
 
-        // TODO Fix visual layout
-        card.add(name, email, requireResponse, remove);
+        var nameAndEmail = new Div(name, email);
+        nameAndEmail.addClassNames("name-and-email");
+
+        var topRow = new Div(nameAndEmail, remove);
+        topRow.addClassNames("row");
+        topRow.setWidthFull();
+
+        card.add(topRow, requireResponse);
+
         return card;
     }
 }
